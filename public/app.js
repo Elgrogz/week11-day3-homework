652a77c9647b1a4fbb8a813d6324c0e0a3e31b25
Elgrogz <gregorgilchrist@gmail.com>
2017-01-25 20:06:18 +0000

    albums.filter working but this.value not returning the correct value

--- public/app.js
+++ public/app.js
@@ -1,7 +1,5 @@
-var data = null;
+var albums = null;
 
-
-//used to get data from a url. Request is created with new XMLHttpRequest, its given a type ('get' because its asking for data) and url to go to. 
 var makeRequest =function(url, callback) {
   var request = new XMLHttpRequest();
   request.open('GET', url);
@@ -9,36 +7,39 @@ var makeRequest =function(url, callback) {
   request.send();
 }
 
-var populateList = function(albums) {
-  var albumList = document.querySelector('#albums');
-
-  albums.forEach(function(album) {
+var populateList = function(albumsToPrint) {
+  var list = document.querySelector('#albums');
+  list.innerText = "";
+  albumsToPrint.forEach(function(album) {
     var p = document.createElement('p');
     p.innerText = "Album title: " + album.name + "\nArtist Name: " + album.artists[0].name + "\nAlbum Type: " + album.album_type;
-    albumList.appendChild(p);
+    list.appendChild(p);
   });
+}
+
+var getAlbumsByArtist = function() {
+  var filteredAlbums = albums.filter(function (album) {
+    console.log(this.value);
+    return (album.artists[0].name === this.value);
+  }.bind(this));
+  console.log(filteredAlbums);
+  populateList(filteredAlbums);
 }
 
 var requestComplete = function() {
   if (this.status != 200) return;
   var jsonString = this.responseText;
-  data = JSON.parse(jsonString);
-  var albums = data.albums.items;
-  console.log(albums);
+  var data = JSON.parse(jsonString);
+  albums = data.albums.items;
   populateList(albums);
 }
-
-
-
 
 var app = function(){
   var url = 'https://api.spotify.com/v1/search?q=christmas&type=album';
   makeRequest(url, requestComplete);
 
-  console.log(data);
-
-  var searchQuery = document.querySelector('#search-query');
-  searchQuery.onclick(showAlbums);
+  var searchQuery = document.querySelector('#search');
+  searchQuery.onclick = getAlbumsByArtist;
 }
 
 window.onload = app;
